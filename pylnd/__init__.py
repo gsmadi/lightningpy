406dfffdd9aadcbcfc619aadcd3c5dd3dbf87fa3
Gabriel Smadi <gsmadi@pm.me>
2019-07-27 14:23:25 -0400

    Base lnd class

---
+++ pylnd/__init__.py
@@ -0,0 +1,17 @@
+from pylnd.abstraction import LNDClientAbstraction
+
+class LNDClientBase(object):
+    """
+    Base class for any LND client implementation to inherit.
+
+    """
+
+    _implementor: LNDClientAbstraction
+    url: str
+    certificate_path: str
+    macaroon_path: str
+
+    def __init__(self, url: str, certificate_path: str, macaroon_path: str):
+        self.url = url
+        self.certificate_path = certificate_path
+        self.macaroon_path = macaroon_path
