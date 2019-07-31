a6f4db708282aecb42f841a3e9fb7a1c9b7bf43c
Gabriel Smadi <gsmadi@pm.me>
2019-07-31 07:25:00 -0400

    Simplify lnd base client

--- pylnd/__init__.py
+++ pylnd/__init__.py
@@ -7,15 +7,10 @@ class LNDClientBase(object):
     """
 
     _implementor: LNDClientAbstraction
-    url: str
-    certificate_path: str
-    macaroon_path: str
 
-    def __init__(self, url: str, certificate_path: str, macaroon_path: str):
-        self.url = url
-        self.certificate_path = certificate_path
-        self.macaroon_path = macaroon_path
-    
+    def __init__(self, implementor):
+        self._implementor = implementor
+
     def get_info(self) -> object:
         response = self._implementor.get_info()
 
