be8cbd654ada78aaaefd1fd101b8a68fc8430bea
Gabriel Smadi <gsmadi@pm.me>
2019-07-31 06:57:07 -0400

    Simplify rest implementation

--- pylnd/rest/__init__.py
+++ pylnd/rest/__init__.py
@@ -22,19 +22,23 @@ class LNDRESTClient(LNDClientAbstraction):
     macaroon_path: str
 
     def __init__(self, url, certificate_path, macaroon_path):
-        macaroon = read_file(certificate_path)
+        self.url = url
+        self.certificate_path = certificate_path
+        self.macaroon_path = macaroon_path
+
+        macaroon = read_file(macaroon_path)
         encoded_macaroon = encode_macaroon(macaroon)
         self.headers = {'Grpc-Metadata-macaroon': encoded_macaroon}
-    
+
     def get_info(self) -> object:
         route = '/v1/getinfo'
 
         return self._get_request(route)
 
-    def _endpoint(self, route):
+    def _endpoint(self, route) -> str:
         return f'{self.url}{route}'
 
-    def _get_request(self, route):
+    def _get_request(self, route) -> object:
         r = requests.get(self._endpoint(route),
                          headers=self.headers,
                          verify=self.certificate_path)
@@ -42,7 +46,7 @@ class LNDRESTClient(LNDClientAbstraction):
 
 class LND(LNDClientBase):
     def __init__(self, url, certificate_path, macaroon_path):
-        super().__init__(url, certificate_path, macaroon_path)
-        self._implementor = LNDRESTClient(url, certificate_path,
-                                          macaroon_path)
+        implementor = LNDRESTClient(url, certificate_path,
+                                    macaroon_path)
+        super().__init__(implementor)
 
