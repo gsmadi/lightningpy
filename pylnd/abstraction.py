d82b5b6772890285e51207b472608c98b352140a
Gabriel Smadi <gsmadi@pm.me>
2019-08-01 07:39:08 -0400

    Additional lnd initialization methods

--- pylnd/abstraction.py
+++ pylnd/abstraction.py
@@ -8,7 +8,29 @@ class LNDClientAbstraction(metaclass=abc.ABCMeta):
         client that an Implementation class must implement.
 
     """
-    
+
     @abc.abstractmethod
-    def get_info(self) -> object:
+    def generate_seed(self,
+                      aezeed_passphrase: str = None,
+                      seed_entropy: str = None) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def info(self) -> object:
+        pass
+
+    @abc.abstractmethod
+    def wallet_init(self,
+                    wallet_password: bytes,
+                    cipher_seed_mnemonic: List[str],
+                    aezeed_passphrase: bytes = None,
+                    recovery_window: int = 0,
+                    channel_backups: object = None) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def wallet_unlock(self,
+                      wallet_password: bytes,
+                      recovery_window: int = 0,
+                      channel_backups: object = None) -> bool:
         pass
