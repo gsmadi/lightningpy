50b80320ed4c0d887737f51ad4d5accf22927716
Gabriel Smadi <gsmadi@pm.me>
2019-08-05 07:10:45 -0400

    Add new client  method tests

--- tests/test_rest_client.py
+++ tests/test_rest_client.py
@@ -14,12 +14,16 @@ class MockResponse:
 
 @pytest.fixture
 def lnd_rest_client(monkeypatch):
-    lnd = LNDRESTClient(URL, CERT_PATH, MACAROON_PATH)
+    lnd = LNDRESTClient(URL, CERT_PATH, MACAROON_PATH, ssl_verify=False)
 
     def mock_get(*args, **kwargs):
         return MockResponse()
 
-    monkeypatch.setattr(requests, "get", mock_get)
+    def mock_post(*args, **kwargs):
+        return MockResponse()
+
+    monkeypatch.setattr(requests, 'get', mock_get)
+    monkeypatch.setattr(requests, 'post', mock_post)
 
     return lnd
 
@@ -27,6 +31,7 @@ def test_rest_client_initialize(lnd_rest_client):
     assert lnd_rest_client.url == URL
     assert lnd_rest_client.certificate_path == CERT_PATH
     assert lnd_rest_client.macaroon_path == MACAROON_PATH
+    assert lnd_rest_client.ssl_verify == False
 
 def test_client_implements_rest(lnd_rest_client):
     assert isinstance(lnd_rest_client, LNDRESTClient)
@@ -41,10 +46,39 @@ def test_client_get_request(lnd_rest_client):
 
     assert result.json().get('mock_key') == 'mock_response'
 
-def test_lnd_get_info(lnd_rest_client):
-    response = lnd_rest_client.get_info()
+def test_client_post_request(lnd_rest_client):
+    result = lnd_rest_client._post_request('/dummy', {'mock': 'param'})
+
+    assert result.json().get('mock_key') == 'mock_response'
+
+def test_lnd_info(lnd_rest_client):
+    response = lnd_rest_client.info()
 
     assert isinstance(response, MockResponse)
+
+def test_lnd_generate_seed(lnd_rest_client):
+    response = lnd_rest_client.generate_seed()
+
+    assert isinstance(response, bool)
+
+    response = lnd_rest_client.generate_seed(aezeed_passphrase='dummy',
+                                             seed_entropy='dummy2')
+
+    assert isinstance(response, bool)
+
+def test_lnd_wallet_init(lnd_rest_client):
+    response = lnd_rest_client.wallet_init(b'password',
+                                             ['str'],
+                                             b'passphrase',
+                                             2)
+
+    assert isinstance(response, bool)
+
+
+def test_lnd_wallet_unlock(lnd_rest_client):
+    response = lnd_rest_client.wallet_unlock(b'password', 2)
+
+    assert isinstance(response, bool)
 
 def test_lnd_client_initialize():
     lnd = LND(URL, CERT_PATH, MACAROON_PATH)
