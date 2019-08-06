fa6aa148aad3c7b8e79ea5e542a34cdda44af0a1
Gabriel Smadi <gsmadi@pm.me>
2019-08-06 07:18:11 -0400

    Prep setup for cicd

--- setup.py
+++ setup.py
@@ -2,24 +2,48 @@
 
 from setuptools import setup, find_packages
 
+def readme():
+    with open("README.md", "r") as readme_file:
+        return readme_file.read()
+
+EXTRAS = {}
+
+EXTRAS['docs'] = {
+    'sphinx'
+}
+
+EXTRAS['dev'] = {
+    'pylint',
+    'pytest',
+    'mypy',
+    'pytest-cov'
+}
+
 setup(
     # Basic info
     name='pylnd',
     version='0.2.3',
     author='Gabriel Smadi',
     author_email='gabriel@labs.smadi.ci',
-    url='https://github.com/smadici-labs/pylnd.git',
+    url='https://github.com/smadici-labs/pylnd',
     description='Python client for Lightning Network Deamon',
-    packages=find_packages(),
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 
     install_requires=[
         'requests',
         'grpcio',
         'grpcio-tools',
         'googleapis-common-protos',
-        'pylint',
-        'pytest',
-        'mypy',
-        'pytest-cov'
     ],
+    extras_require=EXTRAS,
+    license='MIT',
 )
